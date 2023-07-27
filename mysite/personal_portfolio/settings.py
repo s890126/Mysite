c54e6e50f935e00007b8293c1b40de464be0ad5a
Tzu Wang <ss26214555@gmail.com>
2023-07-27 11:39:58 +0800

    Update settings.py

--- mysite/personal_portfolio/settings.py
+++ mysite/personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v%!hkk-4_r-cam61k#x5+0z$kwp&az_w%3cy-+k6(+=8+@&b4g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [''127.0.0.1', '.vercel.app'']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
